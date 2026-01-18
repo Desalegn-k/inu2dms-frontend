106cdddfb83789ce51e56def247c7d3af44ec253
Desalegn Kerie <desu2464@gmail.com>
2026-01-18 11:26:37 +0300

    hash routor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import StudentRegister from './pages/StudentRegister';
 import Navbar from './componenets/Navbar';
 import Footter from './componenets/Footter';
